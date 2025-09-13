c813977b25ea4099f839423862c560d2e668682b
Ziloka <50429450+ziloka@users.noreply.github.com>
2025-09-13 13:22:43 -0400

    random stuff

--- src/main/java/com/example/schoolio/ServingWebContentApplication.java
+++ src/main/java/com/example/schoolio/ServingWebContentApplication.java
@@ -1,13 +1,13 @@
-package com.example.schoolio;
+// package com.example.schoolio;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ServingWebContentApplication {
+// @SpringBootApplication
+// public class ServingWebContentApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ServingWebContentApplication.class, args);
-    }
+//     public static void main(String[] args) {
+//         SpringApplication.run(ServingWebContentApplication.class, args);
+//     }
 
-}
+// }
